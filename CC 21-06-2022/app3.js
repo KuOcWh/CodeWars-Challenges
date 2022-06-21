21e57b9d06cba9ea811d7e762eea499d7b49702a
KuOcWh <kdoescoding@gmail.com>
2022-06-21 15:27:56 +0100

    'question answered'

--- CC 21-06-2022/app3.js
+++ CC 21-06-2022/app3.js
@@ -1,1 +1,18 @@
 //Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(x) {
+    // your code here
+    let lowered = x.toLowerCase()
+    console.log(lowered)
+    
+    let reversedStr = x.split('').reverse().join('')
+    let revLowered = reversedStr.toLowerCase()
+    if(lowered === revLowered ){
+      return true
+    }else{
+      return false
+    }
+  }
+
+// if case sensetivity wasn't an issue 
+  // const isPalindrome = s => === s.split('').reverse.join('') ? console.log('true) : console.log('false')
