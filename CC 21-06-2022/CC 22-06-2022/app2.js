7ef25f349e3b81495db74c0805b8979c761f4d64
KuOcWh <kdoescoding@gmail.com>
2022-06-22 11:04:09 +0100

    'question answered'

--- CC 21-06-2022/CC 22-06-2022/app2.js
+++ CC 21-06-2022/CC 22-06-2022/app2.js
@@ -6,3 +6,13 @@
 //Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
 
 
+function cockroachSpeed(s) {
+    //Good Luck!
+    
+    
+    //the sum for converting km per hour to cm per second
+    let result = s * 27.778
+    
+    //return the result floored.
+    return Math.floor(result)
+  }
