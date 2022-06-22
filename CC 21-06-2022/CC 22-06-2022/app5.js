6e92b25d214e6f1a406d893d32a94ce5710322b5
Kurtis <kdoescoding@gmail.com>
2022-06-22 22:15:21 +0100

    'question answered'

--- CC 21-06-2022/CC 22-06-2022/app5.js
+++ CC 21-06-2022/CC 22-06-2022/app5.js
@@ -1,3 +1,7 @@
 //In this kata you will create a function that takes in a list and returns a list with the reverse order.
 
 
+function reverseList(list) {
+    let reversed = list.reverse()
+    return reversed
+    }
